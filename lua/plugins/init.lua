c7f7457a1b2cbd481671510bf8a8d9b5e3d50f27
Colton Arthur Allen <allen.colton@hotmail.com>
2024-04-10 19:34:15 -0700

    changed colorscheme to everforest

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,7 +6,19 @@ return {
         lazy = false,    -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
-            vim.cmd.colorscheme("night-owl")
+            -- vim.cmd.colorscheme("night-owl")
+        end,
+    },
+    {
+        "neanias/everforest-nvim",
+        version = false,
+        lazy = false,
+        priority = 1000, -- make sure to load this before all the other start plugins
+        -- Optional; default configuration will be used if setup isn't called.
+        config = function()
+            require("everforest").setup({
+                vim.cmd.colorscheme("everforest")
+            })
         end,
     },
     {
